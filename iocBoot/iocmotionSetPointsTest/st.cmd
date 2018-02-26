470c4dc9dd48137bc3e1d28dc7fe5869eda5ece8
John-Holt-Tessella <John.Holt@tessella.com>
2018-02-26 17:23:15 +0000

    make independent motion set points use different lookups

--- iocBoot/iocmotionSetPointsTest/st.cmd
+++ iocBoot/iocmotionSetPointsTest/st.cmd
@@ -23,8 +23,15 @@ motionSetPointsConfigure("LOOKUPFILE1","LOOKUPFILE1")
 epicsEnvSet("LOOKUPFILE1D","${TOP}/iocBoot/${IOC}/lookup1D.txt")
 motionSetPointsConfigure("LOOKUPFILE1D","LOOKUPFILE1D")
 
+epicsEnvSet("LOOKUPFILE1DAXIS","${TOP}/iocBoot/${IOC}/lookup1D.txt")
+motionSetPointsConfigure("LOOKUPFILE1DAXIS","LOOKUPFILE1DAXIS")
+
 epicsEnvSet("LOOKUPFILE2D","${TOP}/iocBoot/${IOC}/lookup2D.txt")
 motionSetPointsConfigure("LOOKUPFILE2D","LOOKUPFILE2D")
+
+epicsEnvSet("LOOKUPFILE2DAXIS","${TOP}/iocBoot/${IOC}/lookup2D.txt")
+motionSetPointsConfigure("LOOKUPFILE2DAXIS","LOOKUPFILE2DAXIS")
+
 
 ## Load our record instances
 dbLoadRecords("${TOP}/db/motionSetPointsTest.db","P=$(MYPVPREFIX)MSP:")
@@ -42,10 +49,10 @@ dbLoadRecords("$(TOP)/db/motionSetPoints.db","P=$(MYPVPREFIX)LKUP:1D:,TARGET_PV1
 dbLoadRecords("$(TOP)/db/motionSetPoints.db","P=$(MYPVPREFIX)LKUP:2D:,TARGET_PV1=$(MYPVPREFIX)MSP:MTR0,TARGET_RBV1=$(MYPVPREFIX)MSP:MTR0.RBV,TARGET_PV2=$(MYPVPREFIX)MSP:MTR1,TARGET_RBV2=$(MYPVPREFIX)MSP:MTR1.RBV,TARGET_DONE=$(MYPVPREFIX)MSP:MTR0.DMOV,TARGET_DONE2=$(MYPVPREFIX)MSP:MTR1.DMOV,TOL=1,LOOKUP=LOOKUPFILE2D")
 
 # 1D with axis
-dbLoadRecords("$(TOP)/db/motionSetPoints.db","P=$(MYPVPREFIX)LKUP:1DAXIS:,AXIS1=$(MYPVPREFIX)AXIS1,TOL=1,LOOKUP=LOOKUPFILE1D")
+dbLoadRecords("$(TOP)/db/motionSetPoints.db","P=$(MYPVPREFIX)LKUP:1DAXIS:,AXIS1=$(MYPVPREFIX)AXIS1,TOL=1,LOOKUP=LOOKUPFILE1DAXIS")
 
 # 2D with axis
-dbLoadRecords("$(TOP)/db/motionSetPoints.db","P=$(MYPVPREFIX)LKUP:2DAXIS:,AXIS1=$(MYPVPREFIX)AXIS1,AXIS2=$(MYPVPREFIX)AXIS2,TOL=1,LOOKUP=LOOKUPFILE2D")
+dbLoadRecords("$(TOP)/db/motionSetPoints.db","P=$(MYPVPREFIX)LKUP:2DAXIS:,AXIS1=$(MYPVPREFIX)AXIS1,AXIS2=$(MYPVPREFIX)AXIS2,TOL=1,LOOKUP=LOOKUPFILE2DAXIS")
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
