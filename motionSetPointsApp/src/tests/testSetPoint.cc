8c18a51c825252ca05156a9fdfa1414b915e43fd
Dominic Oram <dominic.oram@stfc.ac.uk>
2020-07-31 15:03:57 +0100

    Added more tests for load file

--- motionSetPointsApp/src/tests/testSetPoint.cc
+++ motionSetPointsApp/src/tests/testSetPoint.cc
@@ -46,21 +46,30 @@ namespace {
         loadFile(&mockFile, testFilename, "TEST");
     }
 
+    void createMockFile(MockFileIO* mockFile, std::string testFileName, std::vector<std::string> linesInFile, bool expectedToGetToEnd=true) {
+        FILE testFile;
+
+        InSequence seq;
+        EXPECT_CALL(*mockFile, Open(StrEq(testFileName), StrEq("rt"))).WillOnce(Return(&testFile));
+
+        for (auto line : linesInFile) {
+            EXPECT_CALL(*mockFile, Read(_, _, _)).WillOnce(SetArgNPointeeTo<0>(line));
+        }
+
+        if (expectedToGetToEnd) {
+            EXPECT_CALL(*mockFile, Read(_, _, _)).WillOnce(ReturnNull());
+        }
+
+        EXPECT_CALL(*mockFile, Close(_)).Times(1).WillOnce(Return(0));
+    }
+
     TEST(setPoint, GIVEN_empty_file_WHEN_loadFile_called_THEN_file_read_and_closed) {
         auto testFilename = "my_file";
         auto envFilename = "TEST";
         MockFileIO mockFile;
-        FILE testFile;
-
-        {
-            InSequence seq;
-
-            EXPECT_CALL(mockFile, Open(StrEq(testFilename), StrEq("rt"))).WillOnce(Return(&testFile));
-
-            EXPECT_CALL(mockFile, Read(_, _, _)).Times(1).WillOnce(ReturnNull());
-
-            EXPECT_CALL(mockFile, Close(_)).Times(1).WillOnce(Return(0));
-        }
+        std::vector<std::string> fileLines = {};
+        
+        createMockFile(&mockFile, testFilename, fileLines);
 
         loadFile(&mockFile, testFilename, envFilename);
     }
@@ -68,19 +77,10 @@ namespace {
     TEST(setPoint, GIVEN_file_with_comments_in_header_WHEN_loadFile_called_THEN_comments_not_read) {
         auto testFilename = "my_file";
         auto envFilename = "TEST";
-        std::string returnLine = "#Position    2";
+        std::vector<std::string> fileLines = { "#Position    2" };
         MockFileIO mockFile;
-        FILE testFile;
 
-        {
-            InSequence seq;
-            EXPECT_CALL(mockFile, Open(StrEq(testFilename), StrEq("rt"))).WillOnce(Return(&testFile));
-
-            EXPECT_CALL(mockFile, Read(_, _, _)).WillOnce(SetArgNPointeeTo<0>(returnLine));
-            EXPECT_CALL(mockFile, Read(_, _, _)).WillOnce(ReturnNull());
-
-            EXPECT_CALL(mockFile, Close(_)).Times(1).WillOnce(Return(0));
-        }
+        createMockFile(&mockFile, testFilename, fileLines);
 
         loadFile(&mockFile, testFilename, envFilename);
 
@@ -92,19 +92,10 @@ namespace {
     TEST(setPoint, GIVEN_file_with_single_row_and_single_coord_WHEN_loadFile_called_THEN_single_row_and_coord_with_correct_data) {
         auto testFilename = "my_file";
         auto envFilename = "TEST";
-        std::string returnLine = "Position    2";
+        std::vector<std::string> fileLines = { "Position    2" };
         MockFileIO mockFile;
-        FILE testFile;
-
-        {
-            InSequence seq;
-            EXPECT_CALL(mockFile, Open(StrEq(testFilename), StrEq("rt"))).WillOnce(Return(&testFile));
-
-            EXPECT_CALL(mockFile, Read(_, _, _)).WillOnce(SetArgNPointeeTo<0>(returnLine));
-            EXPECT_CALL(mockFile, Read(_, _, _)).WillOnce(ReturnNull());
-
-            EXPECT_CALL(mockFile, Close(_)).Times(1).WillOnce(Return(0));
-        }
+        
+        createMockFile(&mockFile, testFilename, fileLines);
 
         loadFile(&mockFile, testFilename, envFilename);
 
@@ -114,6 +105,89 @@ namespace {
         ASSERT_EQ(table.rows[0].x, 2);
 
         ASSERT_EQ(getNumCoords(envFilename), 1);
+    }
+
+    TEST(setPoint, GIVEN_file_with_single_row_and_two_coords_WHEN_loadFile_called_THEN_single_row_and_coord_with_correct_data) {
+        auto testFilename = "my_file";
+        auto envFilename = "TEST";
+        std::vector<std::string> fileLines = { "Position    2    9" };
+        MockFileIO mockFile;
+        
+        createMockFile(&mockFile, testFilename, fileLines);
+
+        loadFile(&mockFile, testFilename, envFilename);
+
+        auto table = getTable(envFilename);
+
+        ASSERT_EQ(table.rows.size(), 1);
+        ASSERT_EQ(table.rows[0].x, 2);
+        ASSERT_EQ(table.rows[0].y, 9);
+
+        ASSERT_EQ(getNumCoords(envFilename), 2);
+    }
+
+    TEST(setPoint, GIVEN_file_with_multiple_rows_and_single_coord_WHEN_loadFile_called_THEN_multiple_rows_and_coord_with_correct_data) {
+        auto testFilename = "my_file";
+        auto envFilename = "TEST";
+        std::vector<std::string> fileLines = { "Position    2", "Position_2    7" };
+        MockFileIO mockFile;
+
+        createMockFile(&mockFile, testFilename, fileLines);
+
+        loadFile(&mockFile, testFilename, envFilename);
+
+        auto table = getTable(envFilename);
+
+        ASSERT_EQ(table.rows.size(), 2);
+        ASSERT_EQ(table.rows[0].x, 2);
+        ASSERT_EQ(table.rows[1].x, 7);
+
+        ASSERT_EQ(getNumCoords(envFilename), 1);
+    }
+
+    TEST(setPoint, GIVEN_file_with_inconsistent_coord_WHEN_loadFile_called_THEN_table_cleared) {
+        auto testFilename = "my_file";
+        auto envFilename = "TEST";
+        std::vector<std::string> fileLines = { "Position    2", "Position_2    7    9" };
+        MockFileIO mockFile;
+
+        createMockFile(&mockFile, testFilename, fileLines, false);
+
+        loadFile(&mockFile, testFilename, envFilename);
+
+        auto table = getTable(envFilename);
+
+        ASSERT_EQ(table.rows.size(), 0);
+    }
+
+    TEST(setPoint, GIVEN_file_with_multiple_positions_with_the_same_name_WHEN_loadFile_called_THEN_table_cleared) {
+        auto testFilename = "my_file";
+        auto envFilename = "TEST";
+        std::vector<std::string> fileLines = { "Position    2", "Position    7" };
+        MockFileIO mockFile;
+
+        createMockFile(&mockFile, testFilename, fileLines, false);
+
+        loadFile(&mockFile, testFilename, envFilename);
+
+        auto table = getTable(envFilename);
+
+        ASSERT_EQ(table.rows.size(), 0);
+    }
+
+    TEST(setPoint, GIVEN_file_with_multiple_names_for_the_same_position_WHEN_loadFile_called_THEN_table_cleared) {
+        auto testFilename = "my_file";
+        auto envFilename = "TEST";
+        std::vector<std::string> fileLines = { "Position    2    8", "Position_1    2    8" };
+        MockFileIO mockFile;
+
+        createMockFile(&mockFile, testFilename, fileLines, false);
+
+        loadFile(&mockFile, testFilename, envFilename);
+
+        auto table = getTable(envFilename);
+
+        ASSERT_EQ(table.rows.size(), 0);
     }
 
 } // namespace
