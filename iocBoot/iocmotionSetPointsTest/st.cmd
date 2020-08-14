a33ae49831c08f7f6481a04580303c46bb7ab049
Dominic Oram <dominic.oram@stfc.ac.uk>
2020-08-14 19:35:50 +0100

    Removed support for bare axes

--- iocBoot/iocmotionSetPointsTest/st.cmd
+++ iocBoot/iocmotionSetPointsTest/st.cmd
@@ -16,18 +16,8 @@ motorSimCreateController("test", 2)
 motorSimConfigAxis("test", 0, 32000, -320000,  0, 0) 
 motorSimConfigAxis("test", 1, 32000, -320000,  0, 0) 
 
-## setpoint lookup files
-epicsEnvSet("LOOKUPFILE1","${TOP}/iocBoot/${IOC}/analyser.txt")
-motionSetPointsConfigure("LOOKUPFILE1","LOOKUPFILE1", 2)
-
-epicsEnvSet("LOOKUPFILE1D","${TOP}/iocBoot/${IOC}/lookup1D.txt")
-motionSetPointsConfigure("LOOKUPFILE1D","LOOKUPFILE1D", 1)
-
 epicsEnvSet("LOOKUPFILE1DAXIS","${TOP}/iocBoot/${IOC}/lookup1D.txt")
 motionSetPointsConfigure("LOOKUPFILE1DAXIS","LOOKUPFILE1DAXIS", 1)
-
-epicsEnvSet("LOOKUPFILE2D","${TOP}/iocBoot/${IOC}/lookup2D.txt")
-motionSetPointsConfigure("LOOKUPFILE2D","LOOKUPFILE2D", 2)
 
 epicsEnvSet("LOOKUPFILE2DAXIS","${TOP}/iocBoot/${IOC}/lookup2D.txt")
 motionSetPointsConfigure("LOOKUPFILE2DAXIS","LOOKUPFILE2DAXIS", 2)
@@ -42,15 +32,6 @@ motionSetPointsConfigure("LOOKUPFILEDP","LOOKUPFILEDP", 2)
 dbLoadRecords("${TOP}/db/motionSetPointsTest.db","P=$(MYPVPREFIX)MSP:")
 dbLoadRecords("$(AXIS)/db/axis.db","P=$(MYPVPREFIX),AXIS=AXIS1,mAXIS=MSP:MTR0")
 dbLoadRecords("$(AXIS)/db/axis.db","P=$(MYPVPREFIX),AXIS=AXIS2,mAXIS=MSP:MTR1")
-
-# Analyser
-dbLoadRecords("$(TOP)/db/motionSetPointsDoubleAxis.db","P=$(MYPVPREFIX)LKUP:MSP:,TARGET_PV1=$(MYPVPREFIX)MSP:MTR0,TARGET_PV2=$(MYPVPREFIX)MSP:MTR1,TARGET_RBV1=$(MYPVPREFIX)MSP:MTR0.RBV,TARGET_RBV2=$(MYPVPREFIX)MSP:MTR1.RBV,TARGET_DONE1=$(MYPVPREFIX)MSP:MTR0.DMOV,TARGET_DONE2=$(MYPVPREFIX)MSP:MTR1.DMOV,TOL=1,LOOKUP=LOOKUPFILE1")
-
-# 1D no axis
-dbLoadRecords("$(TOP)/db/motionSetPointsSingleAxis.db","P=$(MYPVPREFIX)LKUP:1D:,TARGET_PV1=$(MYPVPREFIX)MSP:MTR0,TARGET_RBV1=$(MYPVPREFIX)MSP:MTR0.RBV,TARGET_DONE1=$(MYPVPREFIX)MSP:MTR0.DMOV,TOL=1,LOOKUP=LOOKUPFILE1D")
-
-# 2D no axis
-dbLoadRecords("$(TOP)/db/motionSetPointsDoubleAxis.db","P=$(MYPVPREFIX)LKUP:2D:,TARGET_PV1=$(MYPVPREFIX)MSP:MTR0,TARGET_PV2=$(MYPVPREFIX)MSP:MTR1,TARGET_RBV1=$(MYPVPREFIX)MSP:MTR0.RBV,TARGET_RBV2=$(MYPVPREFIX)MSP:MTR1.RBV,TARGET_DONE1=$(MYPVPREFIX)MSP:MTR0.DMOV,TARGET_DONE2=$(MYPVPREFIX)MSP:MTR1.DMOV,TOL=1,LOOKUP=LOOKUPFILE2D")
 
 # 1D with axis
 dbLoadRecords("$(TOP)/db/motionSetPointsSingleAxis.db","P=$(MYPVPREFIX)LKUP:1DAXIS:,AXIS1=$(MYPVPREFIX)AXIS1,TOL=1,LOOKUP=LOOKUPFILE1DAXIS")
