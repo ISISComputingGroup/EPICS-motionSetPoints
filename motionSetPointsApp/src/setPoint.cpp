a217281c314f750aa25c018b7e66adb4e3f7b621
Nikola Roev <nikola.roev@stfc.ac.uk>
2022-07-29 11:35:58 +0100

    Update motionSetPointsApp/src/setPoint.cpp
    Co-authored-by: Hannah Cawley <102601525+boeuf-codes@users.noreply.github.com>

--- motionSetPointsApp/src/setPoint.cpp
+++ motionSetPointsApp/src/setPoint.cpp
@@ -150,7 +150,7 @@ void loadFile(FileIOInterface *fileIO, const char *fname, const char *env_fname,
     table.error = "No error";
     // "fname" is the full path to the file, and as there is a limit of 40 characters, extract just the file name.
     std::string fnameString = fname;
-    size_t lastSeparator = fnameString.find_last_of("\\/", std::string::npos, 2);
+    size_t lastSeparator = fnameString.find_last_of("\\/");
     if (lastSeparator != std::string::npos) {
         fnameString = fnameString.substr(lastSeparator + 1);
     }
